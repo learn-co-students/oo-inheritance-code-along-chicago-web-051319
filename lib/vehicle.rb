3de9c01511324f21df6deaddb73fce6283eae334
Martin Spicuzza <spicuzza157@gmail.com>
2019-05-17 04:22:47 +0000

    Done.

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,3 +1,35 @@
+require_relative "./vehicle.rb"
 class Vehicle
+  
+  attr_reader :wheel_size , :wheel_number
+  
+  def initialize(size , number)
+    @wheel_size = size
+    @wheel_number = number
+  end
+  
+  def wheel_size
+    @wheel_size
+  end
+  
+  def wheel_size=(num)
+    @wheel_size = num
+  end
 
+  
+  def wheel_number
+    @wheel_number
+  end
+  
+  def wheel_number=(num)
+    @wheel_number = num
+  end
+    
+  def go
+    return "vrrrrrrrooom!"
+  end
+  
+  def fill_up_tank
+    return "filling up!"
+  end
 end
